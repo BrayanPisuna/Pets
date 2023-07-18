094499b69c87be53fb76b2123f28fa55e31ab80e
David <David@DESKTOP-8DMMD27>
2023-07-18 00:12:54 -0500

    Validacion

--- Pets/Pets/App.xaml.cs
+++ Pets/Pets/App.xaml.cs
@@ -6,11 +6,21 @@ namespace Pets
 {
     public partial class App : Application
     {
+
+        private static string URL = "https://mascota-app.onrender.com/";
+
+        private static string TOKEN_FIRMA = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7";
+
+        public static string TOKEN;
+
+        
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage( new Login());
+            //MainPage = new NavigationPage( new Login(URL , TOKEN_FIRMA));
+            MainPage = new NavigationPage(new Validacion());
         }
 
         protected override void OnStart()
--- Pets/Pets/Login.xaml.cs
+++ Pets/Pets/Login.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +16,56 @@ namespace Pets
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
-        public Login()
+        string url;
+        string secret;
+
+        public Login(string url , string secret)
         {
             InitializeComponent();
+            this.url = url;
+            this.secret = secret;
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+
+
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.Headers[HttpRequestHeader.ContentType] = "application/json";
+
+                Usuario user = new Usuario(lblUser.Text, lblPass.Text);
+
+                string json = JsonConvert.SerializeObject(user);
+
+                App.TOKEN = cliente.UploadString(this.url + "login", "POST", json);
+
+                var mensaje = "Dato ingresado con exito";
+                var validacion = Encoding.ASCII.GetBytes(secret);
+
+                var handler = new JwtSecurityTokenHandler();
+
+                var parametros = new TokenValidationParameters();
+
+                
+
+                DisplayAlert("ALERTA",parametros.ToString() , "OK");
+
+                //DependencyService.Get<Mensaje>().longAlert(mensaje);
+
+                Navigation.PushAsync(new Validacion());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ok", ex.Message, "OK");
+                //DependencyService.Get<Mensaje>().longAlert(ex.Message);
+            }
+
+
+
         }
     }
 }
